c7065d52aaa6a6df75f759661d057106cddf9770
Steven Bitner <sbitner@uwf.edu>
2021-09-01 20:44:03 -0500

    Adds iostream and removes friend to fix compile errors. 	- Also adds Makefile rule for building main from nasty.cpp

--- week-2/student.cpp
+++ week-2/student.cpp
@@ -7,7 +7,8 @@ Student::Student (double gpa, std::string fname, std::string lname)
 	this->fname = fname;
 	this->lname = lname;
 }
-friend std::ostream& operator<< (std::ostream& foo, const Student& me)
+
+std::ostream& operator<< (std::ostream& foo, const Student& me)
 {
 	foo << me.lname << ", " << me.fname << ": " << me.gpa;
 
